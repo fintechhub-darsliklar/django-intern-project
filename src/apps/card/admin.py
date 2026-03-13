a9d0d97ede5b27354c513fc571e7a41e4f30b8c0
fintechhub <ziyodullodev@gmail.com>
2026-03-13 21:47:46 +0500

    add: import-export format

--- src/apps/card/admin.py
+++ src/apps/card/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Card
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from import_export.exceptions import FieldError
+from import_export.exceptions import FieldError, ImportError
+import re
 # Register your models here
 
 class CardResource(resources.ModelResource):
@@ -10,11 +11,51 @@ class CardResource(resources.ModelResource):
     class Meta:
         model = Card
 
-    
+    def normalize_date(self, x):
+        x = str(x).strip()
+
+        if re.match(r"^\d{4}-\d{2}-\d{2}$", x):
+            return x
+
+        elif re.match(r"^\d{4}-\d{2}$", x):
+            return f"{x}-01"
+
+        elif re.match(r"^\d{2}\.\d{4}$", x):
+            m, y = x.split(".")
+            return f"{y}-{m}-01"
+
+        elif re.match(r"^\d{2}/\d{2}$", x):
+            m, y = x.split("/")
+            return f"20{y}-{m}-01"
+
+        return x
+
     def before_import_row(self, row, **kwargs):
-        print(row)
-        raise FieldError("bu xato")
-        # return super().before_import_row(row, **kwargs)
+        if "card_number" not in row:
+            raise ImportError("card_number field bo'lishi majburiy!")
+        elif "phone" not in row:
+            raise ImportError("phone field bo'lishi majburiy!")
+        card_number = str(row.get("card_number"))
+        card_number = card_number.replace("-", "").replace(" ", "")
+        if len(card_number) != 16:
+            raise ImportError(f"Karta raqam 16 xonadan iborat emas! shu malumot: {card_number}")
+        phone_number = str(row.get("phone"))
+        phone_number = phone_number.replace("-", "").replace(" ", "")
+        if not phone_number.startswith("+"):
+            phone_number = "+" + phone_number
+        if not phone_number.startswith("+998"):
+            phone_number = "+998" + phone_number[1:]
+        if len(phone_number) != 13:
+            raise ImportError(f"Telefon raqam notog'ri formatda. togri format; +998901234567! sizdagi malumot: {phone_number}")
+        expired_date = str(row.get("expired"))
+        expired_date = self.normalize_date(expired_date)
+        balance = str(row.get("balance"))
+        balance = balance.replace(" ", "").replace(",", "")
+        row['balance'] = balance
+        row['phone'] = phone_number
+        row['card_number'] = card_number
+        row['expired'] = expired_date
+        return super().before_import_row(row, **kwargs)
     
 
 @admin.register(Card)
